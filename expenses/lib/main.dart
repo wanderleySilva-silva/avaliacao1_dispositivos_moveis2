09ae8fcf49be5da967d9226b30b60c8b044b612a
wanderleySilva-silva <jwps.17@hotmail.com>
2023-10-23 12:11:36 -0300

    Widget Switch adicionado ao projeto

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 import 'package:expenses/components/chart.dart';
@@ -29,47 +28,52 @@ class ExpensesApp extends StatelessWidget {
                 ),
               ),
           appBarTheme: AppBarTheme(
-            toolbarTextStyle: ThemeData.light().textTheme.copyWith(
+            toolbarTextStyle: ThemeData.light()
+                .textTheme
+                .copyWith(
                   headline6: TextStyle(
                     fontFamily: 'OpenSans',
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
-                ).bodyText2, titleTextStyle: ThemeData.light().textTheme.copyWith(
+                )
+                .bodyText2,
+            titleTextStyle: ThemeData.light()
+                .textTheme
+                .copyWith(
                   headline6: TextStyle(
                     fontFamily: 'OpenSans',
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
-                ).headline6,
-          ), colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(secondary: Colors.amber),
+                )
+                .headline6,
+          ),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.amber),
         ));
   }
 }
 
-class StartPage extends StatelessWidget{
-
+class StartPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Bem-vindo'),
       ),
-      body: Center(
-        child: Builder(
-          builder: (BuildContext newContext){
-            return ElevatedButton(
-            onPressed: (){
-              Navigator.of(newContext).push(
-                MaterialPageRoute(builder: (context) => MyHomePage(),
-                ),
-              );
-            },
-          child: Text('Cadastrar transação'),
+      body: Center(child: Builder(builder: (BuildContext newContext) {
+        return ElevatedButton(
+          onPressed: () {
+            Navigator.of(newContext).push(
+              MaterialPageRoute(
+                builder: (context) => MyHomePage(),
+              ),
             );
-          }
-        )
-      ),
+          },
+          child: Text('Cadastrar transações'),
+        );
+      })),
     );
   }
 }
@@ -80,6 +84,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isDarkMode = false; // Linha adicionada
+
   final List<Transaction> _transactions = [];
 
   List<Transaction> get _recentTransactions {
@@ -90,7 +96,8 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  _addTransaction(String categoria, String title, double value, DateTime date) {// String categoria adicinada
+  _addTransaction(String categoria, String title, double value, DateTime date) {
+    // String categoria adicinada
     final newTransaction = Transaction(
       Random().nextDouble().toString(),
       categoria, // Linha adicionada
@@ -122,51 +129,65 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Minhas Despesas'),
-        actions: [
-          IconButton(
-              onPressed: () => _openTransactionFormModal(context),
-              icon: Icon(Icons.add))
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            ListTile(
-              title: Text('Formulário'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Tela inicial'),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                  builder: (context) => StartPage(),
-                  ),
-                  );
-              },
-            ),
+    final ThemeData theme =
+        isDarkMode ? ThemeData.dark() : ThemeData.light(); // Linha adicionada
+
+    return MaterialApp(
+      theme: theme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Minhas Despesas'),
+          actions: [
+            IconButton(
+                onPressed: () => _openTransactionFormModal(context),
+                icon: Icon(Icons.add))
           ],
         ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              ListTile(
+                title: Text('Formulário'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Tela inicial'),
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => StartPage(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Chart(_recentTransactions),
+                TransactionList(_transactions, _removeTransaciton),
+                Switch(
+                    value: isDarkMode, // Linha adicionada
+                    onChanged: (value) {
+                      // Linha adicionada
+                      setState(() {
+                        // Linha adicionada
+                        isDarkMode = value; // Linha adicionada
+                      }); // Linha adicionada
+                    }), //// Linha adicionada
+              ]),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => _openTransactionFormModal(context),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
-
-      body: SingleChildScrollView(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Chart(_recentTransactions),
-              TransactionList(_transactions,_removeTransaciton),
-            ]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => _openTransactionFormModal(context),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
